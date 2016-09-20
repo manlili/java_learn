9cd4ada76653ff0e44d7577ca06454abdee974e0
manlili <manlili@lechebang.com>
2016-09-20 13:58:09 +0800

    第一次提交

---
+++ helloworld/src/com/lechebang/www/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lechebang.www;
+
+/**
+ * Created by manlili on 2016/9/20.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+}
